a353f383773dbb6142572f64a8f0a05f76a14232
Tran Thi Hai Van (Hava) <tthvan.hava@gmail.com>
2026-06-24 16:35:18 +0700

    chore: change comment to code

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -4,24 +4,24 @@
 - Time Complexity: O(N^2) - using 2 nested loops
 - Space Complextiy: O(1) - no extra memory utilized
 */
-// class Solution
-// {
-//     public:
-//     vector<int> twoSum(vector<int>& nums, int target)
-//     {
-//         for (int i = 0; i < nums.size(); i++)
-//         {
-//             int complement = target - nums[i];
+class Solution
+{
+    public:
+    vector<int> twoSum(vector<int>& nums, int target)
+    {
+        for (int i = 0; i < nums.size(); i++)
+        {
+            int complement = target - nums[i];
 
-//             for (int j = i + 1; j < nums.size(); j++)
-//             {
-//                 if (nums[j] == complement)
-//                     return {i, j};
-//             }
-//         }
-//         return {};
-//     }
-// };
+            for (int j = i + 1; j < nums.size(); j++)
+            {
+                if (nums[j] == complement)
+                    return {i, j};
+            }
+        }
+        return {};
+    }
+};
 
 /*
 =====================================APPROACH 2: OPTIMIZED============================
