f5e6a218beb9af625136738c87022138628553c9
Tran Thi Hai Van (Hava) <tthvan.hava@gmail.com>
2026-06-25 20:23:43 +0700

    Time: 0 ms (100%), Space: 11.7 MB (79.7%) - LeetHub

---
+++ 0014-longest-common-prefix/0014-longest-common-prefix.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string res = "";
+
+        if (strs.size() == 0)
+            return res;
+
+        for (int i = 0; i < strs[0].size(); i++)
+        {
+            char c = strs[0][i];
+            for (int j = 1; j < strs.size(); j++)
+            {
+                if (i == strs[j].size() || c != strs[j][i])
+                    return res;
+            }
+            res += c;
+        }
+        return res;
+    }
+};
