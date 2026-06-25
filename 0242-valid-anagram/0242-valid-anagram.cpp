64dae4aa5ef4d10b51706618bc6663132591f59d
Tran Thi Hai Van (Hava) <tthvan.hava@gmail.com>
2026-06-25 17:41:53 +0700

    Time: 0 ms (100%), Space: 10 MB (11.16%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,29 +1,53 @@
-class Solution {
-public:
-    bool isAnagram(string s, string t) {
-        if (s.size() != t.size())
-            return false;
+// class Solution {
+// public:
+//     bool isAnagram(string s, string t) {
+//         if (s.size() != t.size())
+//             return false;
 
         
-        vector<int> sfreq(26, 0);
-        vector<int> tfreq(26, 0);
+//         vector<int> sfreq(26, 0);
+//         vector<int> tfreq(26, 0);
 
-        for (auto c : s)
-        {
-            sfreq[static_cast<int>(c) - 97]++;
-        }
+//         for (auto c : s)
+//         {
+//             sfreq[static_cast<int>(c) - 97]++;
+//         }
 
-        for (auto c : t)
+//         for (auto c : t)
+//         {
+//             tfreq[static_cast<int>(c) - 97]++;
+//         }
+
+//         for (int i = 0; i < 26; i++)
+//         {
+//             if (sfreq[i] != tfreq[i])
+//                 return false;
+//         }
+        
+//         return true;
+//     }
+// };
+
+// Optimize time and space complexity: 
+class Solution {
+public: 
+    bool isAnagram(string s, string t)
+    {
+        if (s.size() != t.size()) return false; 
+
+        vector<int> freq(26, 0);
+
+        for (int i = 0; i < s.size(); i++)
         {
-            tfreq[static_cast<int>(c) - 97]++;
+            freq[s[i] - 'a']++; 
+            freq[t[i] - 'a']--;
         }
 
         for (int i = 0; i < 26; i++)
         {
-            if (sfreq[i] != tfreq[i])
-                return false;
+            if (freq[i] != 0) return false;
         }
-        
+
         return true;
     }
 };
