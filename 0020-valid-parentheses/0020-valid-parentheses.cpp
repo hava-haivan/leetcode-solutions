f252ead92f6eabb78eddb0f7d588647365d63f47
Tran Thi Hai Van (Hava) <tthvan.hava@gmail.com>
2026-06-25 18:23:03 +0700

    Time: 0 ms (100%), Space: 10.1 MB (6.54%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+
+        for (auto c : s)
+        {
+            if ( c == '(')
+                st.push(')');
+            else if ( c == '[')
+                st.push(']');
+            else if ( c == '{')
+                st.push('}');
+            else if (!st.empty() && c == st.top())
+                st.pop();
+            else 
+                return false;
+        }
+
+        return st.empty();
+    }
+};
